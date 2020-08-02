557014ab77c513f6dfa3ff49021cb0592bc9ac14
margaretalice <margaretwoods1982@gmail.com>
2020-08-02 12:06:13 +0000

    . updated

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'summer_peaches'
+app.config["MONGO_URI"] = 'mongodb+srv://margaret:alicia@myfirstcluster-yzvdc.mongodb.net/summer_peaches?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("tasks.html", recipes=mongo.db.recipes.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
 
 
 if __name__ == '__main__':
